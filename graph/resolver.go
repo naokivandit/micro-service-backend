4738c6c2f7821acc274137272f36e5f390e3c102
naokivandit <n.i.vandit0924@gmail.com>
2022-02-23 20:54:11 +0900

    first commit

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/naokivandit/gqlgen-todos/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
