791ae62b868f28605499d93063eb7a3d33dae48e
naokivandit <n.i.vandit0924@gmail.com>
2022-02-24 21:52:01 +0900

    fix

--- helloworld_api/go.mod
+++ helloworld_api/go.mod
@@ -8,8 +8,9 @@ require (
 	github.com/matryer/moq v0.2.3 // indirect
 	github.com/mitchellh/mapstructure v1.2.3 // indirect
 	github.com/rs/cors v1.8.2
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/urfave/cli/v2 v2.3.0 // indirect
 	github.com/vektah/gqlparser/v2 v2.2.0
 	golang.org/x/tools v0.1.5 // indirect
+	google.golang.org/grpc v1.44.0
+	google.golang.org/grpc/examples v0.0.0-20220223191555-e601f1ae37aa
 )
--- helloworld_api/internal/presentaion/helloworld.resolvers.go
+++ helloworld_api/internal/presentaion/helloworld.resolvers.go
@@ -5,17 +5,57 @@ package presentaion
 
 import (
 	"context"
-	"fmt"
+	"flag"
+	"log"
+	"time"
 
-	generated1 "github.com/naokivandit/micro-service-backend/helloworld_api/api/graphql/generated"
-	model1 "github.com/naokivandit/micro-service-backend/helloworld_api/internal/domain/model"
+	"github.com/naokivandit/micro-service-backend/helloworld_api/api/graphql/generated"
+	"github.com/naokivandit/micro-service-backend/helloworld_api/internal/domain/model"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-func (r *queryResolver) SayHello(ctx context.Context, name *string) (*model1.HelloReply, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *queryResolver) SayHello(ctx context.Context, name *string) (*model.HelloReply, error) {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	defer cancel()
+	res, err := c.SayHello(ctx, &pb.HelloRequest{Name: "name"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", res.GetMessage())
+
+	return &model.HelloReply{
+		Message: res.Message,
+	}, nil
 }
 
-// Query returns generated1.QueryResolver implementation.
-func (r *Resolver) Query() generated1.QueryResolver { return &queryResolver{r} }
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
+)
